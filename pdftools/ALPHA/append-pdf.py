3e237befe57b40984e933fc67a0d954db202836c
micheal97 <79813148+micheal97@users.noreply.github.com>
2021-03-01 01:07:38 +0100

    Update append-pdf.py
    just added 'with open' instead of  'open'

--- pdftools/ALPHA/append-pdf.py
+++ pdftools/ALPHA/append-pdf.py
@@ -6,7 +6,11 @@ x = [a for a in os.listdir() if a.endswith(".pdf")]
 merger = PdfFileMerger()
 
 for pdf in x:
-    merger.append(open(pdf, 'rb'))
+    with open(pdf, 'rb') as pdf:                #just added this to close the file again
+        merger.append(pdf)                      #else there was no problem for me. 
+
+#if i put some pdf files into this directory and put PyPDF2 there, too, it should work fine
 
 with open("result.pdf", "wb") as fout:
     merger.write(fout)
+    
